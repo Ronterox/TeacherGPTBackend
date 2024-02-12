7de7f3c964709cfecfadd689bd8f65012948277a
Ricardo <ricarditomontserrat@gmail.com>
2024-02-12 13:50:09 +0100

    Can read pdfs

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.21.6
 require (
 	github.com/PullRequestInc/go-gpt3 v1.2.0
 	github.com/joho/godotenv v1.5.1
+	github.com/ledongthuc/pdf v0.0.0-20240201131950-da5b75280b06
 )
--- main.go
+++ main.go
@@ -5,9 +5,20 @@ import (
 )
 
 func main() {
-	completion, err := gpt("What is the meaning of life?")
+	fileName := "tema_3.1._gestión_de_la_memoria_paginación_y_segmentación.pdf"
+	dirname := "samples/"
+
+	text, err := readPdfByRows(dirname + fileName)
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println(completion)
+
+	log.Println(text)
+
+	// completion, err := gpt("What is the meaning of life?")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// log.Println(completion)
 }
